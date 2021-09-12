c8981558ced9fb777d6a773bce74efe4dffb3380
Spring Wedlund <wedlundspring@gmail.com>
2021-09-12 15:45:30 -0700

    updating files

--- static/js/app.js
+++ static/js/app.js
@@ -25,6 +25,7 @@ function buildTable(data) {
 
 // 1. Create a variable to keep track of all the filters as an object.
 var filters = {};
+
 // 3. Use this function to update the filters. 
 function updateFilters() {
 
@@ -37,6 +38,7 @@ function updateFilters() {
 
       // 4c. Save the id of the filter that was changed as a variable.
   let filterId = changedElement.attr("id");
+  console.log(filterId);
 
       // 5. If a filter value was entered then add that filterId and value
     // to the filters list. Otherwise, clear that filter from the filters object.
@@ -47,29 +49,33 @@ function updateFilters() {
     delete filters[filterId];
     }
 
-    // 6. Call function to apply all filters and rebuild the table
-   filterTable();
+    // Display the filters list on the console.
+  console.log(filters);
+
+  // 6. Call function to apply all filters and rebuild the table
+    filterTable();
   
-}
-  
+  }
+
   // 7. Use this function to filter the table when data is entered.
   function filterTable() {
-  
-    // 8. Set the filtered data to the tableData.
-    var filteredData = tableData;
-  
-    // 9. Loop through all of the filters and keep any data that
+
+  // 8. Set the filtered data to the tableData.
+  let filteredData = tableData;
+
+  // 9. Loop through all of the filters and keep any data that
     // matches the filter values
-    Object.entries(filters).forEach(([key, value]) => {
-      filterdData = filteredData.filter(row => row[key] === value);
+    Object.entries(filters).forEach(([key,value]) => {
+      filteredData = filteredData.filter(row => row[key] === value);
     });
-  
+
     // 10. Finally, rebuild the table using the filtered data
     buildTable(filteredData);
   }
   
   // 2. Attach an event to listen for changes to each filter
   d3.selectAll("input").on("change", updateFilters);
-  
+
   // Build the table when the page loads
-  buildTable(tableData);
+  buildTable(tableData)
+
